e57135d27ff6681d5304901e48fcb9b8db9f422e
YauLim <ayaulym.oralbay@gmail.com>
2025-11-05 12:17:45 +0500

    hw9

---
+++ Composite/Program.cs
@@ -0,0 +1,109 @@
+﻿public abstract class FileSystemComponent
+{
+    public string _name;
+    public FileSystemComponent(string name)
+    {
+        _name = name;
+    }
+    public abstract void Display(int depth);
+    public abstract long GetSize();
+}
+
+public class File : FileSystemComponent
+{
+    private readonly long _size;
+    public File(string name, long size) : base(name)
+    {
+        _size = size;
+    }
+    public override void Display(int depth)
+    {
+        Console.WriteLine($"{new string(' ', depth * 2)}- Файл: {_name} (Размер: {_size} байт)");
+    }
+    public override long GetSize()
+    {
+        return _size;
+    }
+}
+
+public class Directory : FileSystemComponent
+{
+    private readonly List<FileSystemComponent> _children = new List<FileSystemComponent>();
+    public Directory(string name) : base(name) { }
+    public void AddComponent(FileSystemComponent component)
+    {
+        if (!_children.Contains(component))
+        {
+            _children.Add(component);
+        }
+        else
+        {
+            Console.WriteLine($"Компонент '{component._name}' уже существует в папке '{_name}'.");
+        }
+    }
+
+    public void RemoveComponent(FileSystemComponent component)
+    {
+        if (_children.Contains(component))
+        {
+             _children.Remove(component);
+        }
+        else
+        {
+            Console.WriteLine($"Компонент '{component._name}' не найден в папке '{_name}'.");
+        }
+    }
+
+    public override void Display(int depth)
+    {
+        Console.WriteLine($"{new string(' ', depth * 2)}+ Папка: {_name} (Общий размер: {GetSize()} байт)");
+
+        foreach (var component in _children)
+        {
+            component.Display(depth + 1);
+        }
+    }
+
+    public override long GetSize()
+    {
+        return _children.Sum(component => component.GetSize());
+    }
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var root = new Directory("Диск C");
+        var documents = new Directory("Документы");
+        var music = new Directory("Музыка");
+        var systemFiles = new Directory("System");
+        var report = new File("Отчет.docx", 1234); 
+        var presentation = new File("Презентация.pptx", 2345); 
+        var song1 = new File("track01.mp3", 3456); 
+        var systemKernel = new File("kernel32.dll", 4567); 
+
+        root.AddComponent(documents);
+        root.AddComponent(music);
+        root.AddComponent(systemFiles);
+        documents.AddComponent(report);
+        documents.AddComponent(presentation);
+        music.AddComponent(song1);
+        
+        var rockMusic = new Directory("Rock");
+        var song2 = new File("music.mp3", 5678); 
+        rockMusic.AddComponent(song2);
+        music.AddComponent(rockMusic);
+        systemFiles.AddComponent(systemKernel);
+        root.Display(0);
+
+        Console.WriteLine($"Общий размер 'Диска C': {root.GetSize()} байт");
+        Console.WriteLine($"Общий размер папки 'Документы': {documents.GetSize()} байт");
+        Console.WriteLine($"Общий размер папки 'Музыка': {music.GetSize()} байт");
+        Console.WriteLine("Попытка добавить 'Отчет_2024.docx' в 'Документы' еще раз:");
+        documents.AddComponent(report);
+        documents.RemoveComponent(presentation);
+        documents.Display(0);
+    }
+}
---
+++ Facade/Program.cs
@@ -0,0 +1,142 @@
+﻿public class TV
+{
+    public void On()
+    {
+        Console.WriteLine("Телевизор включен");
+    }
+    public void Off()
+    {
+        Console.WriteLine("Телевизор выключен");
+    }
+    public void SelectChannel(int channel)
+    {
+        Console.WriteLine($"Телевизор: выбран канал {channel}");
+    }
+    public void SetAudioInput()
+    {
+        Console.WriteLine("Телевизор: установлен аудиовход для прослушивания музыки");
+    }
+}
+
+public class AudioSystem
+{
+    public void On()
+    {
+        Console.WriteLine("Аудиосистема включена");
+    }
+    public void Off()
+    {
+        Console.WriteLine("Аудиосистема выключена");
+    }
+    public void SetVolume(int volume)
+    {
+        Console.WriteLine($"Аудиосистема: громкость установлена на {volume}");
+    }
+}
+
+public class DVDPlayer
+{
+    public void Play()
+    {
+        Console.WriteLine("DVD-проигрыватель: воспроизведение");
+    }
+    public void Pause()
+    {
+        Console.WriteLine("DVD-проигрыватель: пауза");
+    }
+    public void Stop()
+    {
+        Console.WriteLine("DVD-проигрыватель: остановка");
+    }
+}
+
+public class GameConsole
+{
+    public void On()
+    {
+        Console.WriteLine("Игровая консоль включена");
+    }
+    public void Off()
+    {
+        Console.WriteLine("Игровая консоль выключена");
+    }
+    public void StartGame()
+    {
+        Console.WriteLine("Игровая консоль: игра запущена");
+    }
+}
+
+public class HomeTheaterFacade
+{
+    private readonly TV _tv;
+    private readonly AudioSystem _audioSystem;
+    private readonly DVDPlayer _dvdPlayer;
+    private readonly GameConsole _gameConsole;
+
+    public HomeTheaterFacade(TV tv, AudioSystem audioSystem, DVDPlayer dvdPlayer, GameConsole gameConsole)
+    {
+        _tv = tv;
+        _audioSystem = audioSystem;
+        _dvdPlayer = dvdPlayer;
+        _gameConsole = gameConsole;
+    }
+
+    public void WatchMovie()
+    {
+        _tv.On();
+        _tv.SelectChannel(5); 
+        _audioSystem.On();
+        _audioSystem.SetVolume(15);
+        _dvdPlayer.Play();
+    }
+    
+    public void TurnEverythingOff()
+    {
+        _dvdPlayer.Stop();
+        _gameConsole.Off();
+        _audioSystem.Off();
+        _tv.Off();
+    }
+    
+    public void StartGaming()
+    {
+        _tv.On();
+        _tv.SelectChannel(3); 
+        _audioSystem.On();
+        _audioSystem.SetVolume(20);
+        _gameConsole.On();
+        _gameConsole.StartGame();
+    }
+    
+    public void ListenToMusic()
+    {
+        _audioSystem.On();
+        _audioSystem.SetVolume(25);
+        _tv.On(); 
+        _tv.SetAudioInput();
+    }
+    
+    public void AdjustVolume(int volume)
+    {
+        _audioSystem.SetVolume(volume);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var tv = new TV();
+        var audioSystem = new AudioSystem();
+        var dvdPlayer = new DVDPlayer();
+        var gameConsole = new GameConsole();
+        var homeTheater = new HomeTheaterFacade(tv, audioSystem, dvdPlayer, gameConsole);
+        
+        homeTheater.WatchMovie();
+        homeTheater.TurnEverythingOff();
+        homeTheater.StartGaming();
+        homeTheater.AdjustVolume(20);
+        homeTheater.ListenToMusic();
+        homeTheater.TurnEverythingOff();
+    }
+}
